9fa1eda2a4688a1f799d54f5fbec370179fbd76b
Joao Guilherme <jguipanza@gmail.com>
2020-03-13 16:48:11 -0300

    feat: Configurando redux saga

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -25,10 +25,10 @@ class Home extends Component {
     this.setState({ products: data });
   }
 
-  handleAddProduct = product => {
-    const { addToCArt } = this.props;
+  handleAddProduct = id => {
+    const { addToCartRequest } = this.props;
 
-    addToCArt(product);
+    addToCartRequest(id);
   };
 
   render() {
@@ -45,7 +45,7 @@ class Home extends Component {
 
             <button
               type="button"
-              onClick={() => this.handleAddProduct(product)}
+              onClick={() => this.handleAddProduct(product.id)}
             >
               <div>
                 <MdAddShoppingCart size={16} color="#FFF" />{' '}
